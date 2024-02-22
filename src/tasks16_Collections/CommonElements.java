a4c552d3d48b47fd8fbcb34bef024a39e08b9daa
mmadly <mmadly2014@gmail.com>
2024-02-22 12:57:16 -0600

    update commonElement day 16

--- src/tasks16_Collections/CommonElements.java
+++ src/tasks16_Collections/CommonElements.java
@@ -6,6 +6,26 @@ public class CommonElements {
 
     public static void main(String[] args) {
 
+
+        Set<Integer> set1 = new TreeSet<>(Arrays.asList(10,8,11,20,5,0));
+        Set<Integer> set2 = new TreeSet<>(Arrays.asList(30,0,20,40,50,10,60));
+
+
+
+        Set<Integer> set3 = new TreeSet<>();
+
+        for (Integer i : set1) {
+            for (Integer integer : set2) {
+                if (i.equals(integer)){
+                    set3.add(i);
+                }
+            }
+        }
+
+        System.out.println(set3);
+
+
+/*
         List<Integer> set1 = new LinkedList<>(Arrays.asList(10,8,11,20,5,0));
         List<Integer> set2 = new LinkedList<>(Arrays.asList(30,0,20,40,50,10,60));
 
@@ -23,6 +43,6 @@ public class CommonElements {
 
         System.out.println(set3);
 
-
+*/
     }
 }
